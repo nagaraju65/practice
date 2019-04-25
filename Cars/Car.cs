65089cfa8db3787a3cd8d48f836b0b9c89624e3f
Nagaraju Salendra <406255@cognizant.com>
2019-04-25 16:28:45 +0530

    Created new Console project Car

---
+++ Cars/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car
+    {
+        public string Model_Year { get; set;}
+        public string Division{get; set;}
+        public string Carline{get; set;}
+        public double Eng_Displ{get; set;}
+        public int Cyl{get; set;}
+        public int FE_City{get; set;}
+        public int FE_Hwy{get; set;}
+        public int FE_Comb{get; set;}
+
+    }
+}
---
+++ Cars/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = ProcessCSV("fuel.csv");
+            foreach (var car in source)
+            {
+                Console.WriteLine(car.Division);
+            }
+        }
+
+        private static List<Car> ProcessCSV(string path)
+        {
+            var query = File.ReadAllLines(path)
+                            .Skip(1)
+                            .Where(line => line.Length > 1)
+                            .Select(ParseStringtoCar)
+                            .ToList();
+            return query;
+        }
+
+        private static Car ParseStringtoCar(string path)
+        {
+            var dr = path.Split(',');
+            return new Car()
+            {
+                 
+                Model_Year = dr[0] ,
+                Division = dr[1] ,
+                Carline = dr[2] ,
+                Eng_Displ = double.Parse(dr[3]) ,
+                Cyl = int.Parse(dr[4]) ,
+                FE_City = int.Parse(dr[5]) ,
+                FE_Hwy = int.Parse(dr[6]) ,
+                FE_Comb = int.Parse(dr[7])
+            };
+        }
+    }
+}
