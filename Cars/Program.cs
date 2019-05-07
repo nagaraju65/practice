31a4976c3dbe498a1949005207012498e69ef450
Laptop <nag.raju65@gmail.com>
2019-05-07 23:02:40 +0530

    Join Practice

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -13,27 +13,60 @@ namespace Cars
         {
             var Cars = ProcessCars("fuel.csv");
             var Manufacturers = ProcessManufacturers("manufacturers.csv");
+            ExecuteFunctionality(Cars, Manufacturers);
+
+        }
+
+        private static void ExecuteFunctionality(List<Car> cars, List<Manufacturer> manufacturers)
+        {
+            var Query = from car in cars
+                        join manufacturer in manufacturers on car.Division equals manufacturer.Name
+                        select new
+                        {
+                            country = manufacturer.HeadQuarter,
+                            name = car.Division,
+                            Combined_FE = car.FE_Comb
+                        };
+
+            foreach (var car in Query)
+            {
+                Console.WriteLine($"{car.country}:{car.name}:{car.Combined_FE}");
+            }
+
+            /*
+            ShowCars(cars);
+            ShowManufacturers(manufacturers);
+            */
+        }
+
+        private static void ShowManufacturers(List<Manufacturer> Manufacturers)
+        {
             foreach (var Manufacturer in Manufacturers)
             {
                 Console.WriteLine($"{Manufacturer.Name} : {Manufacturer.HeadQuarter}");
             }
+        }
 
-            //foreach (var car in Cars)
-            //{
-            //    Console.WriteLine(car.Division);
-            //}
+        private static void ShowCars(List<Car> Cars)
+        {
+
+
+            foreach (var car in Cars)
+            {
+                Console.WriteLine(car.Division);
+            }
         }
 
         private static List<Manufacturer> ProcessManufacturers(string path)
         {
             var Query = File.ReadAllLines(path)
                             .Where(line => line.Length > 1)
-                            .Select(Manufacturer.ConvertLineToManufacturer);
-                            //.OrderBy(m => m.Name);
+                            .Select(Manufacturer.ConvertLineToManufacturer)
+                            .OrderBy(m => m.Name);
 
             return Query.ToList();
 
-            
+
         }
 
         private static List<Car> ProcessCars(string path)
@@ -51,14 +84,14 @@ namespace Cars
             var dr = path.Split(',');
             return new Car()
             {
-                 
-                Model_Year = dr[0] ,
-                Division = dr[1] ,
-                Carline = dr[2] ,
-                Eng_Displ = double.Parse(dr[3]) ,
-                Cyl = int.Parse(dr[4]) ,
-                FE_City = int.Parse(dr[5]) ,
-                FE_Hwy = int.Parse(dr[6]) ,
+
+                Model_Year = dr[0],
+                Division = dr[1],
+                Carline = dr[2],
+                Eng_Displ = double.Parse(dr[3]),
+                Cyl = int.Parse(dr[4]),
+                FE_City = int.Parse(dr[5]),
+                FE_Hwy = int.Parse(dr[6]),
                 FE_Comb = int.Parse(dr[7])
             };
         }
